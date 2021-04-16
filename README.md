7c4b2ccc8cc529f8a0516ccea3ef947c8553cdc1
shj3497 <shj3497@gmail.com>
2021-04-16 19:25:42 +0900

    Postman을 통한 데이터 전송

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Node.js 기초 React.js 기초
 
 - npm install express --save
 - npm install mongoose --save
+- npm install body-parser --save
+
+- 회원가입 테스트 : PostMan 활용
--- index.js
+++ index.js
@@ -3,6 +3,16 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+const bodyParser = require('body-parser');
+
+const { User } = require('./models/User');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
 // use~ 써주는 이유 : 안써주면 에러가 가끔 날때가 있음.
 mongoose.connect(`mongodb+srv://shj3497:rkawkrhrnak1!@node-react.j15yu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
@@ -14,5 +24,25 @@ mongoose.connect(`mongodb+srv://shj3497:rkawkrhrnak1!@node-react.j15yu.mongodb.n
 });
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/register', (req, res) => {
+
+    // 회원가입시 필요한 정보들을 client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    // 현재 user는 User.js에서 가져온 객체이다.
+    const user = new User(req.body);
+    // .save() : db에 저장
+    user.save((err, userInfo) => {
+        if(err) {
+            return res.json({success:false, err})
+        }else{
+            return res.status(200).json({
+                success:true
+            })
+        }
+        
+    });
+})
 
 app.listen(port, ()=> util.log(`Example app listening on port ${port}!`));
